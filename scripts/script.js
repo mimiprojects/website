56bcc0f2e49087a45235f8ad388c7e80d1826fc0
mimiprojects <mimiszeto@gmail.com>
2015-08-01 22:01:48 -0400

    fixed lightbox

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,9 @@
-'use strict';
 
-(function($) {
   $(document).ready(function() {
-    $('.nav').on('click', function() {
-    	$(this).toggleClass('is-active');
-    });
-    $('#nav-toggle').on('click', function(eventObject) {
-    	eventObject.preventDefault();
-    	$(this).add('#menu').toggleClass('open');
-    });
-  });
-})(jQuery);
+	$('.image-popup-link').magnificPopup({ 
+  		type: 'image'
+	// other options
+	});
+});
+
+
