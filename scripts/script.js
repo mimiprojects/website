9d51b6ba152fdae171c095fe46835f5ca6867954
mimiprojects <mimiszeto@gmail.com>
2015-08-01 23:26:50 -0400

    Hamburger Menu
    Tried to add a hamburger menu

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,16 @@
   $(document).ready(function() {
 	$('.image-popup-link').magnificPopup({ 
   		type: 'image'
-	// other options
 	});
+
+	$('#nav-toggle').on('click', function() {
+    	$(this).toggleClass('is-active');
+    });
+    
+    $('#nav-toggle').on('click', function(eventObject) {
+    	eventObject.preventDefault();
+    	$(this).add('#menu').toggleClass('is-open');
+    });
 });
 
 
